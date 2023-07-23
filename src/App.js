3559d69b19688e30bfd37d1e940335b51e550c29
MrsGuzel <aleevgzll@gmail.com>
2023-07-23 23:11:38 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css"
+import Home from "./pages/Home"
+// import {AiOutlineDelete} from "react-icons/ai"
+
+const App = () => {
+  return(
+    <div>
+      <Home />
+      {/* <AiOutlineDelete /> */}
+    </div>
+  )
+   
+}
+
+export default App
---
+++ src/components/AppointmentList.jsx
@@ -0,0 +1,35 @@
+import { Col, Row } from "react-bootstrap";
+import Container from "react-bootstrap/Container"
+import { TiDelete } from "react-icons/ti"
+const AppointmentList = ({apps}) => {
+  console.log(apps);
+  return (
+    <Container className="p-2">
+      <h3 className="display-6 mb-2" style={{ color: "rgb(166, 18, 189)" }}>
+        Appointment List
+      </h3>
+
+      {apps.map(({id, patient, consulted, doctor, day}) => (
+      <div 
+      key={id} 
+      className={consulted ? "appointments consulted" : "appointments"}>
+
+        <Row className="justify-content-between align-item-center">
+          <Col xs={12} sm={12} md={6}>
+            <h4>{patient}</h4>
+            <h4>{doctor}</h4>
+          </Col>
+          <Col>
+          <h5>{day}</h5>
+          </Col>
+          <Col className="text-end">
+          <TiDelete className="text-danger fs-1"/>
+          </Col>
+        </Row>
+      </div>
+      ))}
+    </Container>
+  )
+}
+
+export default AppointmentList
